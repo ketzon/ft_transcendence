f065b63d66930011f01c18cde238bdff09ea0cae
Parallels <francois.gnz@gmail.com>
2025-04-04 06:49:53 +0200

    updates and profile view template

--- ui/spa/main.js
+++ ui/spa/main.js
@@ -1,10 +1,19 @@
 const routes = {
+    index : "/",
     dashboard : "/dashboard",
-    profile : "/profile"
+    profile : "/profile",
+    play : "/play"
 }
 
+// On recupere le body pour injecter le contenu dans cette partie.
 const body = document.querySelector("body");
 
+// Gestion des boutons forward et backward
+window.addEventListener("popstate", () => {
+    router();
+});
+
+//On ajoute des event listener sur tout les boutons.
 function handleLinks() {
     const links = document.querySelectorAll("a");
     console.log(links);
@@ -19,12 +28,20 @@ function handleLinks() {
     }
 }
 
+
+//On injecte le contenu selon le path sur lequel on se trouve.
 async function router() {
+    //On injecte dans changingArea pour garder la navbar sur la gauche dans le body.
     const changingArea = document.getElementById("changingArea");
     console.log(changingArea);
 
     console.log(location.pathname);
     switch (location.pathname) {
+        case routes.index:
+            body.innerHTML = await (await fetch("./pages/landing.html")).text();
+            handleLinks();
+            break;
+
         case routes.dashboard:
             body.innerHTML = await (await fetch("./pages/dashboard.html")).text();
             handleLinks();
@@ -35,9 +52,15 @@ async function router() {
             handleLinks();
             break ;
 
+        case routes.play:
+            changingArea.innerHTML = await (await fetch("./pages/play.html")).text()
+            handleLinks();
+            break ;
+
         default:
             break ;
     }
 }
 
 handleLinks();
+router();

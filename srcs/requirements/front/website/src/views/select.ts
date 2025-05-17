e291da7b057a0b5079c17b273ef2f99751a3f37e
florian <bessonflorian42@yahoo.fr>
2025-05-17 10:41:11 +0200

    adjust map full screen

--- srcs/requirements/front/website/src/views/select.ts
+++ srcs/requirements/front/website/src/views/select.ts
@@ -3,7 +3,8 @@ import { background } from "./pong"
 
 export function selectView(): string {
   return /*html*/`
-    <div id="custom-prompt" class=" ${background} bg-no-repeat bg-center bg-contain bg-white/60 min-h-screen flex justify-center items-center bg-opacity-50 hidden">
+
+    <div id="custom-prompt" class=" ${background} bg-no-repeat bg-center bg-[length:100%_100%] min-h-screen flex justify-center items-center bg-opacity-50 hidden">
       <div class="bg-white opacity-85 p-6 rounded-lg shadow-xl max-w-md w-full">
         <h3 id="prompt-title" class="text-xl font-bold text-purple-800 mb-4">Enter Username</h3>
         <input 

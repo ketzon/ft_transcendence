34b42e084b2717daaebba56b4427883a006de785
François GONZALEZ <francois.gnz@gmail.com>
2025-05-21 15:05:15 +0200

    improving ux for otp

--- srcs/requirements/front/website/src/views/2fa.ts
+++ srcs/requirements/front/website/src/views/2fa.ts
@@ -57,7 +57,79 @@ function verifyCode(userEmail: string): void {
     }
 }
 
+//Auto switch when a input to the next input when keypress.
+function handleAutoswitch(): void {
+    const inputs = document.getElementById("inputs");
+
+    inputs?.addEventListener("input", (e) => {
+        const target = e.target;
+
+        if (target && target instanceof HTMLInputElement)
+        {
+            const val = target?.value;
+            if (isNaN(val)) // Act as verify that we have a number in value.
+            {
+                target.value = "";
+                return ;
+            }
+            if (val !== "")
+            {
+                const next = target.nextElementSibling;
+                if (next && next instanceof HTMLInputElement)
+                    next.focus();
+            }
+        }
+    })
+}
+
+//Handle delete a input value and go back to previous input
+function handleDeleteKeys(): void {
+    const inputs = document.getElementById("inputs");
+
+    inputs?.addEventListener("keyup", (e) => {
+        const target = e.target;
+        const key = e.key.toLocaleLowerCase();
+
+        if (key == "backspace" || key == "delete")
+        {
+            if (target && target instanceof HTMLInputElement)
+            {
+                target.value = "";
+                const prev = target.previousElementSibling;
+
+                if (prev && prev instanceof HTMLInputElement)
+                    prev.focus();
+            }
+            return;
+        }
+    })
+}
+
+//Handle if a value is already in the input to replace it then focus next.
+function handleInputReplace() {
+    const allInputs = document.querySelectorAll("input");
+
+    for (let i = 0; i < allInputs.length; i++)
+    {
+        allInputs[i].addEventListener("keyup", (e) => {
+            if (allInputs[i].value === "")
+            {
+                return;
+            }
+            else if (!isNaN(e.key))
+            {
+                allInputs[i].value = e.key;
+                const next = allInputs[i].nextElementSibling as HTMLInputElement | null;
+                next?.focus();
+            }
+        })
+    }
+}
+
 export function init2fa(userEmail: string): void {
+    handleAutoswitch();
+    handleDeleteKeys();
+    handleInputReplace();
     verifyCode(userEmail);
 }
 
@@ -67,7 +139,7 @@ export function twofaView(userEmail: string):string {
             <form id="twofa-form" class="flex flex-col items-center justify-center gap-9 rounded-2xl w-[460px] h-[600px] bg-white shadow relative">
                <span class="text-2xl font-bold">Enter OTP</span>
                <p class="text-center text-gray-600">We have sent a verification code to your email address</p>
-               <div class="w-full flex gap-2.5 items-center justify-center">
+               <div id="inputs" class="w-full flex gap-2.5 items-center justify-center">
                     <input required maxlength="1" type="text" class="bg-gray-200 w-10 h-10 border-0 rounded-md text-center font-bold outline-0" id="otp-input1" name="otp">
                     <input required maxlength="1" type="text" class="bg-gray-200 w-10 h-10 border-0 rounded-md text-center font-bold outline-0" id="otp-input2" name="otp">
                     <input required maxlength="1" type="text" class="bg-gray-200 w-10 h-10 border-0 rounded-md text-center font-bold outline-0" id="otp-input3" name="otp">

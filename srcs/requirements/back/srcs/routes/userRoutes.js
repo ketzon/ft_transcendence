41a0f2977762e88044fae2e2b9362151ff54b4cd
Aether Guez <theaethersky@gmail.com>
2025-05-30 19:02:30 +0200

    style: fix indentation

--- srcs/requirements/back/srcs/routes/userRoutes.js
+++ srcs/requirements/back/srcs/routes/userRoutes.js
@@ -2,18 +2,18 @@ import userManagement from "../controllers/users_controller.js"
 import friendsController from "../controllers/friends_controller.js";
 
 export default async function registerUserRoute(fastify) {
-	fastify.post("/signup", userManagement.signup)
-	fastify.post("/signin", userManagement.signin)
-	fastify.post("/logout", userManagement.logout)
-	fastify.post("/customUsername", userManagement.customUsername)
-	fastify.post("/customAvatar", userManagement.customAvatar)
-	fastify.post("/modifyPassword", userManagement.modifyPassword)
-	fastify.post("/verify-2FA", userManagement.verify2FA)
-	fastify.get("/profil", userManagement.displayCurrentUser)
-    fastify.post("/language", userManagement.updateLanguage);
-    fastify.post("/resendOtpCode", userManagement.resendOtpCode);
+	fastify.post("/signup", userManagement.signup);
+	fastify.post("/signin", userManagement.signin);
+	fastify.post("/logout", userManagement.logout);
+	fastify.post("/customUsername", userManagement.customUsername);
+	fastify.post("/customAvatar", userManagement.customAvatar);
+	fastify.post("/modifyPassword", userManagement.modifyPassword);
+	fastify.post("/verify-2FA", userManagement.verify2FA);
+	fastify.get("/profil", userManagement.displayCurrentUser);
+  fastify.post("/language", userManagement.updateLanguage);
+  fastify.post("/resendOtpCode", userManagement.resendOtpCode);
 
-    fastify.post("/friends/add", friendsController.addFriend);
-    fastify.get("/friends/list", friendsController.getFriends);
-    fastify.delete("/friends/remove/:id", friendsController.removeFriend);
+  fastify.post("/friends/add", friendsController.addFriend);
+  fastify.get("/friends/list", friendsController.getFriends);
+  fastify.delete("/friends/remove/:id", friendsController.removeFriend);
 }

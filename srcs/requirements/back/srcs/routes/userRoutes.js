b6589a6aa6f545d122f6f79277a8dffe6b4d9f65
Aether Guez <theaethersky@gmail.com>
2025-05-30 19:19:08 +0200

    feat: add delusr route to autoloaded routes

--- srcs/requirements/back/srcs/routes/userRoutes.js
+++ srcs/requirements/back/srcs/routes/userRoutes.js
@@ -5,6 +5,7 @@ export default async function registerUserRoute(fastify) {
 	fastify.post("/signup", userManagement.signup);
 	fastify.post("/signin", userManagement.signin);
 	fastify.post("/logout", userManagement.logout);
+  fastify.post("/delusr", userManagement.deleteUser);
 	fastify.post("/customUsername", userManagement.customUsername);
 	fastify.post("/customAvatar", userManagement.customAvatar);
 	fastify.post("/modifyPassword", userManagement.modifyPassword);
